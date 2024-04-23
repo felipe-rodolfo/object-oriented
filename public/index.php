2922a2a018ed0efcf70749e6bc02ede735046f32
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-04-23 20:59:49 -0300

    inheritance

--- public/index.php
+++ public/index.php
@@ -3,13 +3,12 @@ declare(strict_types=1);
 
 require '../vendor/autoload.php';
 
-use app\classes\Login;
+use app\classes\Book;
 
+$book = new Book();
+$book->name = 'Entendendo Algoritmos: Um Guia Ilustrado Para Programadores e Outros Curiosos';
+$book->descritpion = 'Um algoritmo nada mais é do que um procedimento passo a passo para a resolução de um problema. Os algoritmos que você mais utilizará como um programador já foram descobertos, testados e provados. Se você quer entendê-los, mas se recusa a estudar páginas e mais páginas de provas, este é o livro certo. Este guia cativante e completamente ilustrado torna simples aprender como utilizar os principais algoritmos nos seus programas. ';
+$book->author = 'Aditya Bhargava';
+$book->pages = 264;
 
-
-try {
-    $login = new Login();
-    echo $login->auth('felipe@email.com', 123);
-} catch(TypeError $e){
-    echo $e->getMessage();
-}
+echo json_encode($book);
