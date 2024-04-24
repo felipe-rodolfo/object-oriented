d75133e14edcf5cfb2b6e47be9eb48bbae998f46
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-04-24 18:11:26 -0300

    magic method __construct

---
+++ app/classes/Person.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\classes;
+
+class Person
+{
+    public function __construct(public string $name, public string $email)
+    {
+       
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,14 +1,11 @@
 <?php
 declare(strict_types=1);
 
+use app\classes\Person;
+
 require '../vendor/autoload.php';
 
-use app\classes\Book;
+$person = new Person('Felipe', 'feliperodolfo.midias@gmail.com');
 
-$book = new Book();
-$book->name = 'Entendendo Algoritmos: Um Guia Ilustrado Para Programadores e Outros Curiosos';
-$book->descritpion = 'Um algoritmo nada mais é do que um procedimento passo a passo para a resolução de um problema. Os algoritmos que você mais utilizará como um programador já foram descobertos, testados e provados. Se você quer entendê-los, mas se recusa a estudar páginas e mais páginas de provas, este é o livro certo. Este guia cativante e completamente ilustrado torna simples aprender como utilizar os principais algoritmos nos seus programas. ';
-$book->author = 'Aditya Bhargava';
-$book->pages = 264;
-
-echo json_encode($book);
+echo $person->email;
+echo $person->name;
