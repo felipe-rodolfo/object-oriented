12eb4fd424c20647cd0aed5d4e2f5edfae487217
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-04-24 18:27:23 -0300

    constructor in parent and child class

--- public/index.php
+++ public/index.php
@@ -1,11 +1,32 @@
 <?php
 declare(strict_types=1);
 
-use app\classes\Person;
-use app\classes\User;
-
 require '../vendor/autoload.php';
 
-$user = new User();
+class User 
+{
+    public string $name;
+    public int $age;
 
-echo $user->info();
+    public function __construct(string $name, int $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getUserInfo()
+    {
+        return $this->name. " - ". $this->age;
+    }
+}
+
+class User2 extends User
+{
+    public function __construct(string $name, int $age)
+    {
+        parent::__construct($name, $age);
+    }
+}
+
+$user = new User2("Felipe", 19);
+echo $user->getUserInfo();
