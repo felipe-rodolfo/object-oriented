afb0051ea7b23e1369a03a9abd42a3945e2a1cec
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-04-24 21:00:54 -0300

    Instantiating a class with static methods

--- public/index.php
+++ public/index.php
@@ -5,28 +5,14 @@ require '../vendor/autoload.php';
 
 class User 
 {
-    public string $name;
-    public int $age;
-
-    public function __construct(string $name, int $age)
+    public static string $name;
+    
+    public static function userInfo()
     {
-        $this->name = $name;
-        $this->age = $age;
+        return "teste método estático";
     }
 
-    public function getUserInfo()
-    {
-        return $this->name. " - ". $this->age;
-    }
+   
 }
 
-class User2 extends User
-{
-    public function __construct(string $name, int $age)
-    {
-        parent::__construct($name, $age);
-    }
-}
-
-$user = new User2("Felipe", 19);
-echo $user->getUserInfo();
+echo User::userInfo();
