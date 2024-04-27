01ebcab95dbeb87eb8779354bc2450edba14c3f1
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-04-27 15:47:59 -0300

    abstract class and methods

--- public/index.php
+++ public/index.php
@@ -3,42 +3,54 @@ declare(strict_types=1);
 
 require '../vendor/autoload.php';
 
-class BankAccount {
-    private $accountNumber;
-    private $balance;
+abstract class Database {
+    protected $host;
+    protected $username;
+    protected $password;
+    protected $databaseName;
+    protected $connection;
 
-    public function __construct($accountNumber, $balance) {
-        $this->accountNumber = $accountNumber;
-        $this->balance = $balance;
+    public function __construct($host, $username, $password, $databaseName) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->databaseName = $databaseName;
     }
 
-    public function getAccountNumber() {
-        return $this->accountNumber;
-    }
-
-    public function getBalance() {
-        return $this->balance;
-    }
-
-    public function deposit($amount) {
-        if($amount > 0){
-            $this->balance += $amount;
-            return "Deposit of $amount sucessfully made. New balance: $this->balance" . PHP_EOL;
-        }
-    }
-
-    public function withdraw($amount) {
-        if($amount > 0 && $amount <= $this->balance){
-            $this->balance -= $amount;
-            return "Withdrawal of $amount sucessfully made. New balance: $this->balance" . PHP_EOL;
-        } else {
-            return "Insufficient funds. Current balance: $this->balance" . PHP_EOL;
-        }
-    }
+    abstract protected function connect();
+    abstract protected function query($sql);
+    abstract protected function close();
 }
 
-$account = new BankAccount(123456789, 1000);
-$account->withdraw(999);
-$account->deposit(14);
-echo "Account Number: " . $account->getAccountNumber() . PHP_EOL;
-echo "Current Balance: " . $account->getBalance() . PHP_EOL;
+class MySQLDatabase extends Database {
+    public function connect() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->databaseName);
+
+        if($this->connection->connect_error){
+            die("Connection failed: ". $this->connection->connect_error);
+        }
+    }
+
+    public function query($sql){
+        $result = $this->connection->query($sql);
+        return $result;
+
+        if(!$result){
+            die("Query failed: ". $this->connection->error);
+        }
+
+        return $result;
+    }
+
+    public function close() {
+        $this->connection->close();
+    }
+
+}
+
+$mysqlDatabase = new MySQLDatabase('localhost', 'username', 'password', 'test');
+
+$mysqlDatabase->connect();
+$sql = "SELECT * FROM users";
+$result = $mysqlDatabase->query($sql);
+$mysqlDatabase->close();
