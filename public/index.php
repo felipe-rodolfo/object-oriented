728134af3ce6ec095b249b95145a997348de04c1
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-04-28 16:01:42 -0300

    polymorphism

--- public/index.php
+++ public/index.php
@@ -1,40 +1,31 @@
 <?php
 declare(strict_types=1);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 require '../vendor/autoload.php';
 
-interface CartInterface {
-    public function add($product);
-    public function remove($product);
-    public function getTotal();
-    public function getProducts();
+abstract class Checkout {
+    abstract public function pay($payment): array;
 }
 
-class Cart implements CartInterface {
-    private $products = [];
-
-    public function add($product) {
-        $this->products[] = $product;
-    }
-
-    public function remove($product) {
-        $this->products = array_values(array_filter($this->products, function($item) use ($product) {
-            return $item->getName()!= $product->getName();
-        }));
-    }
-
-    public function getTotal() {
-        $total = 0;
-        foreach ($this->products as $product) {
-            $total += $product->getPrice();
-        }
-        return $total;
-    }
-
-    public function getProducts() {
-        return $this->products;
+class PaypalCheckout extends Checkout {
+    public function pay($payment): array {
+        return [
+            'PaypalCheckout payment' => $payment
+        ];
     }
 }
+
+class PagseguroCheckout extends Checkout {
+
+    public function pay($payment): array {
+        return [
+            'PagseguroCheckout payment' => $payment
+        ];
+    }
+}
+
+$paypal = new PaypalCheckout;
+$paypal->pay(120);
+
+$pagseguro = new PagseguroCheckout;
+$pagseguro->pay(150);
