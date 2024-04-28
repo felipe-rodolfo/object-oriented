c922f3c6f6637fb07aa8517f9900422c61ab1710
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-04-28 15:34:08 -0300

    method overriding

--- public/index.php
+++ public/index.php
@@ -3,54 +3,29 @@ declare(strict_types=1);
 
 require '../vendor/autoload.php';
 
-abstract class Database {
-    protected $host;
-    protected $username;
-    protected $password;
-    protected $databaseName;
-    protected $connection;
-
-    public function __construct($host, $username, $password, $databaseName) {
-        $this->host = $host;
-        $this->username = $username;
-        $this->password = $password;
-        $this->databaseName = $databaseName;
-    }
-
-    abstract protected function connect();
-    abstract protected function query($sql);
-    abstract protected function close();
+abstract class Checkout {
+    abstract public function pay($payment): array;
 }
 
-class MySQLDatabase extends Database {
-    public function connect() {
-        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->databaseName);
-
-        if($this->connection->connect_error){
-            die("Connection failed: ". $this->connection->connect_error);
-        }
+class PaypalChecout extends Checkout {
+    public function pay($payment): array {
+        return [
+            'Paypal payment' => $payment
+        ];
     }
-
-    public function query($sql){
-        $result = $this->connection->query($sql);
-        return $result;
-
-        if(!$result){
-            die("Query failed: ". $this->connection->error);
-        }
-
-        return $result;
-    }
-
-    public function close() {
-        $this->connection->close();
-    }
-
 }
 
-$mysqlDatabase = new MySQLDatabase('localhost', 'username', 'password', 'test');
+class PagseguroCheckout extends Checkout {
+    public function pay($payment): array {
+        return [
+            'Pagseguro payment' => $payment
+        ];
+    }
+}
 
-$mysqlDatabase->connect();
-$sql = "SELECT * FROM users";
-$result = $mysqlDatabase->query($sql);
-$mysqlDatabase->close();
+
+$payment = new PaypalChecout;
+$payment->pay(120);
+foreach ($payment as $key => $value) {
+    echo $key. ': '. $value. '<br>';
+}
