8099411d5ce76bd16ba40f46ffc8741d02aae735
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2024-04-28 21:24:47 -0300

    dependency injection

--- public/index.php
+++ public/index.php
@@ -3,48 +3,22 @@ declare(strict_types=1);
 
 require '../vendor/autoload.php';
 
-
-class Employee {
-    private $name;
-    private $position;
-
-    public function __construct($name, $position) {
-        $this->name = $name;
-        $this->position = $position;
+class Notifier {
+    public function notify() {
+        echo "Notify users";
     }
+}
+class Post {
+    public function __construct(
+        public string $title,
+        public Notifier $notifier
+        ) {}
 
-    public function getName() {
-        return $this->name;
-    }
-
-    public function getPosition() {
-        return $this->position;
+    public function publish() {
+        echo "Publishing post";
+        $this->notifier->notify();
     }
 }
 
-class Department {
-    private $name;
-    private $employees = [];
-
-    public function __construct($name) {
-        $this->name = $name;
-    }
-
-    public function addEmployee(Employee $employee) {
-        $this->employees[] = $employee;
-    }
-
-    public function getEmployees() {
-        return $this->employees;
-    }
-}
-
-$employee1 = new Employee("John Doe", "Manager");
-$employee2 = new Employee("Jane Smith", "Developer");
-
-$department = new Department("IT Department");
-
-$department->addEmployee($employee1);
-$department->addEmployee($employee2);
-
-$employeesInDepartment = $department->getEmployees();
+$newPost = new Post("Learning depency injecyion", new Notifier);
+$newPost->publish();
